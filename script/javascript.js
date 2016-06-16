003dbd1b519255f26bd811a6338f859f99283bb1
Jason Hsieh <JasonHsieh@dhcp-v073-096.mobile.uci.edu>
2016-06-16 15:35:58 -0700

    added function for appending new cards

--- script/javascript.js
+++ script/javascript.js
@@ -1,3 +1,34 @@
 /**
  * Created by JasonHsieh on 6/14/16.
  */
+
+var editCardContainer;
+var cards = [
+    {
+        title: 'My new card',
+        notes: "My notes"
+    }
+];
+
+
+
+$(function() {
+    editCardContainer = $('#edit_card');
+
+    $('#add_card_btn').on('click', function() {
+        editCardContainer.removeClass('hide');
+    });
+
+    $('.cancel_button').on('click', function() {
+        $('.card').addClass('hide');
+    });
+
+    $('#save_button').on('click', function() {
+        cards.push({title: $('#new_card_title')[0].value, notes: $('#new_card_notes')[0].value});
+        $('#new_card_title')[0].value = null;
+        $('#new_card_notes')[0].value = null;
+        editCardContainer.addClass('hide');
+        console.log(cards);
+    });
+
+});
