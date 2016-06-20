047297ee484ce06e44fa543eac82ead89ee79820
jasonh117 <jasonh117@hotmail.com>
2016-06-19 22:35:55 -0700

    added title to preview cards

--- script/javascript.js
+++ script/javascript.js
@@ -3,28 +3,7 @@
  */
 
 var editCardContainer;
-var cards = [
-    {
-        title: 'My new card',
-        notes: "My notes"
-    }
-];
-
-var new_card="";
-new_card += " <div class=\"preview\">";
-new_card += "    <p class=\"title\">title<\/p>";
-new_card += "    <div class=\"card-tags\">";
-new_card += "    <ul>";
-new_card += "    <li>Homework<\/li>";
-new_card += "    <li>Science<\/li>";
-new_card += "    <li>PE<\/li>";
-new_card += "    <\/ul>";
-new_card += "    <\/div>";
-new_card += "    <img src=\"http:\/\/dummyimage.com\/250x220\/87CEFA\/fff\">";
-new_card += "    <div class=\"preview_date\">mm-dd-yyyy<\/div>";
-new_card += "    <\/div>";
-
-
+var cards = [];
 
 $(function() {
     editCardContainer = $('#edit_card');
@@ -38,13 +17,28 @@ $(function() {
     });
 
     $('#save_button').on('click', function() {
+        if ($('#new_card_title')[0].value === "")
+            $('#new_card_title')[0].value = "No Title";
+        
+        var new_card=" ";
+        new_card += "<div class=\"preview\">";
+        new_card += "    <p class=\"title\">" + $('#new_card_title')[0].value + "<\/p>";
+        new_card += "    <div class=\"card-tags\">";
+        new_card += "    <ul>";
+        new_card += "    <li>Homework<\/li>";
+        new_card += "    <li>Science<\/li>";
+        new_card += "    <li>PE<\/li>";
+        new_card += "    <\/ul>";
+        new_card += "    <\/div>";
+        new_card += "    <img src=\"http:\/\/dummyimage.com\/250x220\/87CEFA\/fff\">";
+        new_card += "    <div class=\"preview_date\">mm-dd-yyyy<\/div>";
+        new_card += "    <\/div>";
+        $("#main_container").prepend(new_card);
+        
         cards.push({title: $('#new_card_title')[0].value, notes: $('#new_card_notes')[0].value});
         $('#new_card_title')[0].value = null;
         $('#new_card_notes')[0].value = null;
-        //editCardContainer.addClass('hide');
-        $("#main_container").prepend(new_card);
-
-        console.log(cards);
+        editCardContainer.addClass('hide');
     });
 
 });
